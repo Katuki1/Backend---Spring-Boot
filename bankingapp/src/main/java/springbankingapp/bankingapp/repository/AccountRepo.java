4d025d41773599d82f4bbe7b829ba7e5de99cc1b
Katuki1 <98540297+Katuki1@users.noreply.github.com>
2024-02-05 14:10:57 +0300

    Before controller class

--- bankingapp/src/main/java/springbankingapp/bankingapp/repository/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package springbankingapp.bankingapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springbankingapp.bankingapp.entity.Account;
-
-public interface AccountRepo extends JpaRepository <Account, Long>{
-}
